922f86f3f80535dea3feb566ba5d5ce81c5e06e6
SPAWNS <P2DUCHEV@ABV.bg>
2016-12-09 23:28:37 +0200

    Fixing youtube video player

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -33,5 +33,24 @@ module ApplicationsHelper
    def check_for_incomplete
     if application_check then true else false end
    end
-  
+
+  def parse_video_url(url)
+    @url = url
+    youtube_formats = [
+        %r(https?://youtu\.be/(.+)),
+        %r(https?://www\.youtube\.com/watch\?v=(.*?)(&|#|$)),
+        %r(https?://www\.youtube\.com/embed/(.*?)(\?|$)),
+        %r(https?://www\.youtube\.com/v/(.*?)(#|\?|$)),
+        %r(https?://www\.youtube\.com/user/.*?#\w/\w/\w/\w/(.+)\b)
+    ]
+    @url.strip!
+    if @url.include? "youtu"
+      youtube_formats.find { |format| @url =~ format } and $1
+      @results = $1
+      @results
+    else
+      return nil
+    end
+  end
+
 end
