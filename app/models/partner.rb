e9ba26c6b154df5b367e621858c6e94e9c1e1550
SPAWNS <P2DUCHEV@ABV.bg>
2016-12-09 18:53:08 +0200

    Adding roles to sponsors

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,7 +1,6 @@
 class Partner < ActiveRecord::Base
     validates_inclusion_of :role  , :in => %w( corporate individual), :message => "has to be either corporate or individual sponsor"
     validates :name, :presence => true
-    validates_format_of :link, :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/
     has_attached_file :logo, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.jpg"
     validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 end
