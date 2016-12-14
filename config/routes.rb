2dd6058fddf437eacd4f076cd10f8989b830c110
SPAWNS <P2DUCHEV@ABV.bg>
2016-12-14 18:10:01 +0200

    Fixing assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   scope(:path => '/aubgquest') do
   devise_for :users, controllers: { :registrations => 'users/registrations' , :sessions => 'users/sessions', confirmations: 'users/confirmations'}
   devise_for :admins, controllers: {:sessions => 'admins/sessions'}
