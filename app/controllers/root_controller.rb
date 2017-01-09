24b08685025012a84beed98d7f3fe8594b2d7dbd
SPAWNS <P2DUCHEV@ABV.bg>
2017-01-09 11:35:20 +0200

    Fixing facebook page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -8,7 +8,7 @@ class RootController < ApplicationController
      #FB
      fboauth = Koala::Facebook::OAuth.new(ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'])
      fbgraph = Koala::Facebook::API.new( fboauth.get_app_access_token)
-     @posts = fbgraph.get_connections("169054876446733" , "posts" , {"limit" => "7"})
+     @posts = fbgraph.get_connections("1897955503824222" , "posts" , {"limit" => "7"})
 
       #YT
      channel = Yt::Channel.new url: "https://www.youtube.com/user/AUBG1991"
