af217155d025548b44ff60504d37caf6bf59a571
SPAWNS <P2DUCHEV@ABV.bg>
2017-01-10 14:16:10 +0200

    Changes in applications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { host: 'https://aubgquest.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'aubgquest.herokuapp.com', :protocol => 'http'}
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
